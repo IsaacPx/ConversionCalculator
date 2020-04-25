7e3d073fbe8543ca17ca47a39d0d4c642f463266
Isaac Pedroza <pedroza3@illinois.edu>
2020-04-24 20:17:05 -0500

    4/24/20 conversion calculator in progress.

--- app/src/main/java/com/example/conversioncalculator/MainActivity.java
+++ app/src/main/java/com/example/conversioncalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.conversioncalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button convertBtn = (Button) findViewById(R.id.convert);
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText inputNumEditText = (EditText) findViewById(R.id.inputConversion);
+                TextView outputNumTextView = (TextView) findViewById(R.id.outputConversion);
+
+                double inputNum = Double.parseDouble(inputNumEditText.getText().toString());
+                double outputNum = inputNum * 2.54;
+                outputNumTextView.setText(outputNum + " cm");
+            }
+        });
     }
 }
